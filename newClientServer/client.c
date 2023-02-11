7b5a2d2f60e0e16a7c29b29c2e598d526c418273
Miconbeko <stepanov.m2004@gmail.com>
2023-02-11 16:45:58 +0200

    feat: add openssl lib

--- newClientServer/client.c
+++ newClientServer/client.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "../../erproc.h"
+#include "erproc.h"
 
 int main() {
     int fd = Socket(AF_INET, SOCK_STREAM, 0);
--- newClientServer/server.c
+++ newClientServer/server.c
@@ -5,7 +5,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include "../../erproc.h"
+#include "erproc.h"
 
 int main() {
     int server = Socket(AF_INET, SOCK_STREAM, 0);
