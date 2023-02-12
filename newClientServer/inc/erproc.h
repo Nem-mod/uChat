b0e56aad92c2e5ce04e7dacd99aec42d43cf709c
Miconbeko <stepanov.m2004@gmail.com>
2023-02-12 15:58:37 +0200

    chore: path changed. fucked up changes in main

--- newClientServer/inc/erproc.h
+++ newClientServer/inc/erproc.h
@@ -3,6 +3,10 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
