91a8f1f5118dac478b0f6c9905a21c8f7b96a9c8
Nem-mod <67012818+Nem-mod@users.noreply.github.com>
2023-04-05 11:51:08 +0300

    feat: headers

--- client/inc/client.h
+++ client/inc/client.h
@@ -25,15 +25,15 @@ typedef struct s_serv_connection {
     SSL* ssl;                       // 
     SSL_CTX* ctx;                   // Context
     int hs_result;                  //
-    pthread_mutex_t listener_mutex; // mutex for listening server
+    pthread_t listener_thread; // mutex for listening server
     char lbuffer[MAXBUFFER];        // buffer for retrive message
-    pthread_mutex_t writer_mutex;   // mutex for writing to server
+    //pthread_t writer_mutex;   // mutex for writing to server
     char wbuffer[MAXBUFFER];        // buffer for writing message
 }               t_serv_connection;
 
 
 /* Struct wich contains GtkWidgets for SignUp Scene*/
-typedef struct s_signupWin {
+typedef struct s_signupScene {
     GtkWidget *w_signup;      // window signup
     GtkWidget *e_f_login;     // entry field {login}
     GtkWidget *e_f_password;  // entry field {password}
@@ -43,12 +43,12 @@ typedef struct s_signupWin {
     GtkWidget *b_signup;      // button signUp
     GtkWidget *bl_signin;     // button-link on signIn window
 
-}              t_signupWin;
+}              t_signupScene;
 
 
 /* Struct wich contain specific app scenes */
 typedef struct s_uchatScenes {
-    t_signupWin* signUp_scene; // signUp scene
+    t_signupScene* signUp_scene; // signUp scene
 
 }              t_uchatScenes;
 
@@ -56,8 +56,8 @@ typedef struct s_uchatScenes {
 /* Struct of client application where main data is stored*/
 typedef struct s_UchatApplication {
     t_serv_connection* serv_conection; // server conection
-    GtkApplication* gtk_app;            // link to Gtk application 
-    GtkCssProvider *css_provider;       // link to CSS provider
+    //GtkApplication* gtk_app;            // link to Gtk application 
+    //GtkCssProvider *css_provider;       // link to CSS provider
     t_uchatScenes* scenes;              // link to gui scenes
     int active_scene;                   // flag which one scene is active
 
@@ -67,3 +67,10 @@ typedef struct s_UchatApplication {
 
 /* Connect to server function*/
 t_serv_connection *mx_init_server_conection(int port);
+void* mx_listen_server(void* data);
+void mx_write_to_server(SSL* ssl, char* buffer);
+
+t_UchatApplication* mx_create_app(char* argv[]);
+GtkWidget *mx_get_widget(GtkBuilder *builder, char *id);
+t_uchatScenes* mx_create_scenes();
+void mx_init_signup(GtkBuilder* builder, t_signupScene* signUpScene);
