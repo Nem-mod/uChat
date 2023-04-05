e526e1dd39ddd97e2accd7d6a2a44d2321193884
Nem-mod <67012818+Nem-mod@users.noreply.github.com>
2023-04-05 11:48:39 +0300

    feat: add getWidget function

---
+++ client/src/mx_get_widget.c
@@ -0,0 +1,7 @@
+#include "client.h"
+
+/* Function get GtkWidget from object by id*/
+GtkWidget *mx_get_widget(GtkBuilder *builder, char *id) {
+    GtkWidget *out = GTK_WIDGET(gtk_builder_get_object(builder, id));
+    return out;
+}
