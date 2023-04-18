b098643d6f63e59426a1df83d2195d1376d3747f
NorthWind08 <zseveryn.lipovetskyz@gmail.com>
2023-04-18 19:34:28 +0300

    fix:add_contact

--- client/src/callbacks/mx_callback_add_contact.c
+++ client/src/callbacks/mx_callback_add_contact.c
@@ -21,7 +21,7 @@ void mx_callback_add_contact(UNUSED GtkButton *button, gpointer data) {
     struct json_object *jobj = json_object_new_object();
     json_object_object_add(jobj, "user_id", json_object_new_int(app->user_id));
     json_object_object_add(jobj, "login", json_object_new_string((char*)login));
-    mx_write_to_server(app->serv_connection->ssl,  mx_create_request("POST","/group/members", jobj));
+    mx_write_to_server(app->serv_connection->ssl,  mx_create_request("POST","/contact/", jobj));
     mx_write_to_server(app->serv_connection->ssl, mx_create_request("GET", "/user/groups", jobj));
     gtk_widget_hide(app->scenes->add_contact_dwindow->w_add_contact);
 
