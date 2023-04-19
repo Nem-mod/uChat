66bcd8d3cf78d4de6db2bbe61a992ff0867b88c1
Miconbeko <stepanov.m2004@gmail.com>
2023-04-19 10:47:06 -0700

    feat: mx_gtk_find_child | feat: changing chat name on click

--- client/src/callbacks/mx_callback_chatbox.c
+++ client/src/callbacks/mx_callback_chatbox.c
@@ -2,7 +2,6 @@
 
 void mx_callback_chatbox(UNUSED GtkButton *button, gpointer data) {
     t_uchat_application* app = (t_uchat_application*)data;
-<<<<<<< HEAD
     //g_print("%s",  gtk_widget_get_name(GTK_WIDGET( button)));
     app->current_group_id = mx_atoi(gtk_widget_get_name(GTK_WIDGET(button)));
     app->last_message_indx = 0;
@@ -11,8 +10,6 @@ void mx_callback_chatbox(UNUSED GtkButton *button, gpointer data) {
 
     gtk_container_foreach(GTK_CONTAINER(app->scenes->chat_scene->l_sc_messages), (GtkCallback)gtk_widget_destroy, NULL);
 
-    mx_write_to_server(app->serv_connection->ssl, mx_create_request("GET", "/group/message", jobj));
-=======
     GtkWidget *chat_label = mx_gtk_find_child(GTK_WIDGET(button), "chat_name");
 
     // mx_log_info(SYSLOG, "current_group_id = vvv");
@@ -23,7 +20,6 @@ void mx_callback_chatbox(UNUSED GtkButton *button, gpointer data) {
     // mx_log_info(SYSLOG, (char*)gtk_label_get_text(GTK_LABEL(chat_label)));
     gtk_label_set_text(GTK_LABEL(app->scenes->chat_scene->l_chatname), gtk_label_get_text(GTK_LABEL(chat_label)));
 
->>>>>>> c353c35 (feat: mx_gtk_find_child | feat: changing chat name on click)
     
 
     struct json_object* jobj = json_object_new_object();
