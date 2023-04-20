a85e2045a438afd75d58fa0449ae41fe00d3f8a5
NemMod <necha.enemy@gmail.com>
2023-04-20 18:56:27 +0300

    fix: incorrect id's

--- client/src/creators/mx_create_app.c
+++ client/src/creators/mx_create_app.c
@@ -3,10 +3,10 @@
 t_uchat_application* mx_create_app(char* argv[]) {
     t_uchat_application* app = malloc(sizeof(t_uchat_application));
 
-    app->user_id = -1;
-    app->current_group_id = -1;
-    app->last_message_id = -1;
-    
+    app->user_id = 0;
+    app->current_group_id = 0;
+    app->last_message_id = 0;
+    app->choosed_file_name = NULL;
     mx_create_scenes(app);
     mx_init_server_connection(app, mx_atoi(argv[1]));
 
