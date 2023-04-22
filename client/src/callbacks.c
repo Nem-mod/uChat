1256eaf713d0266e0f43ddf16cd607bcb250f698
NemMod <necha.enemy@gmail.com>
2023-04-22 22:33:59 +0300

    fix: gtk warning

--- client/src/callbacks.c
+++ client/src/callbacks.c
@@ -345,17 +345,18 @@ void mx_callback_patch_user(UNUSED GtkButton *button, gpointer data) {
             json_object_object_add(jobj, "file_name", json_object_new_string(filename));
             json_object_object_add(jobj, "file_size", json_object_new_uint64(file_size));
         }
+        mx_set_image_widget_size(GTK_IMAGE(app->scenes->chat_scene->img_user), 
+        (app->scenes->chat_scene->img_user),  
+        app->choosed_file_pname);
+
+        if(app->choosed_file_pname != NULL) {
+                mx_strdel(&app->choosed_file_pname);
+        }
     }
     
     
     mx_write_to_server(app->serv_connection->ssl,  mx_create_request("PATCH","/user/", jobj));
-    mx_set_image_widget_size(GTK_IMAGE(app->scenes->chat_scene->img_user), 
-        (app->scenes->chat_scene->img_user),  
-        app->choosed_file_pname);
-
-    if(app->choosed_file_pname != NULL) {
-            mx_strdel(&app->choosed_file_pname);
-    }
+    
 }
 
 
